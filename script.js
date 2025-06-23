2a613e852d402a15a8ba81315d6f9550408bc77a
counter185 <33550839+counter185@users.noreply.github.com>
2025-06-23 21:59:35 +0200

    initial

---
+++ script.js
@@ -0,0 +1,62 @@
+
+links = [
+    { name: "Welcome", url: "manual/landing.html", subpages: [
+        { name: "Open-source licenses", url: "manual/under_construction.html", subpages: [] },
+    ] },
+    { name: "Getting started", url: "manual/under_construction.html", subpages: [
+        { name: "Supported formats", url: "manual/under_construction.html", subpages: [] },
+        { name: "Workspaces", url: "manual/under_construction.html", subpages: [] },
+    ] },
+    { name: "voidsprite Launchpad", url: "manual/under_construction.html", subpages: [
+        { name: "About color modes", url: "manual/under_construction.html", subpages: [] },
+        { name: "Templates", url: "manual/under_construction.html", subpages: [] },
+        { name: "Quick Convert", url: "manual/under_construction.html", subpages: [] },
+        { name: "Recovery autosaves", url: "manual/under_construction.html", subpages: [] },
+    ] },
+    { name: "Editor", url: "manual/under_construction.html", subpages: [
+        { name: "Tools", url: "manual/under_construction.html", subpages: [] },
+        { name: "Patterns", url: "manual/under_construction.html", subpages: [] },
+        { name: "Spritesheet preview", url: "manual/under_construction.html", subpages: [] },
+        { name: "3D Cube preview", url: "manual/under_construction.html", subpages: [] },
+        { name: "RPG Maker 2000/2003 map preview", url: "manual/under_construction.html", subpages: [] },
+    ] },
+    { name: "Split sessions", url: "manual/under_construction.html", subpages: [] },
+    { name: "Multiple windows", url: "manual/under_construction.html", subpages: [] },
+    { name: "Settings", url: "manual/under_construction.html", subpages: [] },
+];
+
+function changePage(link) {
+    iframe_content.src = link;
+}
+
+function linkElement(name, link) {
+    var aElem = document.createElement("a");
+    aElem.href = link;
+    aElem.textContent = name;
+    aElem.onclick = function(event) {
+        event.preventDefault();
+        changePage(link);
+    };
+    return aElem;
+}
+
+function evalLinks(pages, parentElement) {
+    pages.forEach(element => {
+        var linkElem = linkElement(element.name, element.url);
+        parentElement.appendChild(linkElem);
+        if (element.subpages) {
+            var subList = document.createElement("div");
+            subList.className = "sub-links";
+            evalLinks(element.subpages, subList);
+            parentElement.appendChild(subList);
+        }
+    });
+}
+
+function load() {
+    div_links = document.getElementById("sidebar-links");
+    iframe_content = document.getElementById("manual-content");
+
+    evalLinks(links, div_links);
+}
+
