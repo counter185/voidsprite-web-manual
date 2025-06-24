d0dc9cb61ef7904c381fa81dc4ddc1c53751b86a
counter185 <33550839+counter185@users.noreply.github.com>
2025-06-24 11:02:40 +0200

    back links

--- manual/singlepage.js
+++ manual/singlepage.js
@@ -1,14 +1,27 @@
 function makeHeader() {
     return `<h4>
-                ◆This page is part of the voidsprite web manual. <a href="../index.html">View full manual</a>
+                ◆This page is part of the voidsprite web manual. <a class="manual-link" href="../index.html">View full manual</a>
             </h4>`;
 }
 
+function makeReturnLink(page) {
+    return `<h4><a class="manual-link" href=${page}.html>
+                Back to previous page
+            </a></h4>`;
+}
+
 function checkIframe() {
+    var insertInto = document.getElementsByClassName("manual-body")[0];
     if (window === window.parent) {
-        var insertInto = document.getElementsByClassName("manual-body")[0];
         if (insertInto) {
             insertInto.innerHTML = makeHeader() + insertInto.innerHTML;
+        }
+    }
+    const queryParams = new URLSearchParams(window.location.search);
+    var returnPage = queryParams.get("back");
+    if (returnPage != null) {
+        if (insertInto) {
+            insertInto.innerHTML = makeReturnLink(returnPage) + insertInto.innerHTML;
         }
     }
 }
