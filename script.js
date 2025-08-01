169f5c4a5adda29f56538db21ed6b9c5c91883ab
counter185 <33550839+counter185@users.noreply.github.com>
2025-08-01 13:12:26 +0200

    page about quick convert, restyle sidebar

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ links = [
     { name: "voidsprite Launchpad", url: "manual/launchpad.html", subpages: [
         { name: "About color modes", url: "manual/under_construction.html", subpages: [] },
         { name: "Templates", url: "manual/templates.html", subpages: [] },
-        { name: "Quick Convert", url: "manual/under_construction.html", subpages: [] },
+        { name: "Quick Convert", url: "manual/quickconvert.html", subpages: [] },
         { name: "Recovery autosaves", url: "manual/under_construction.html", subpages: [] },
     ] },
     { name: "Editor", url: "manual/under_construction.html", subpages: [
@@ -28,32 +28,44 @@ links = [
     ] },
 ];
 
+createdLinkElements = [];
+
+function deactivateAllLinkElements() {
+    createdLinkElements.forEach(function(linkElem) {
+        linkElem.classList.remove("sublink-active");
+    });
+}
+
 function changePage(link) {
     iframe_content.src = link;
 }
 
-function linkElement(name, link) {
+function linkElement(name, link, subLevel) {
     var aElem = document.createElement("a");
     aElem.href = link;
+    aElem.classList.add("sublink-level-" + subLevel);
     if (link == "manual/under_construction.html") {
-        aElem.className = "redlink";
+        aElem.classList.add("redlink");
     }
     aElem.textContent = name;
     aElem.onclick = function(event) {
         event.preventDefault();
         changePage(link);
+        deactivateAllLinkElements();
+        aElem.classList.add("sublink-active");
     };
     return aElem;
 }
 
-function evalLinks(pages, parentElement) {
+function evalLinks(pages, parentElement, subLevel) {
     Array.prototype.forEach.call(pages, function(element) {
-        var linkElem = linkElement(element.name, element.url);
+        var linkElem = linkElement(element.name, element.url, subLevel);
         parentElement.appendChild(linkElem);
+        createdLinkElements.push(linkElem);
         if (element.subpages) {
             var subList = document.createElement("div");
             subList.className = "sub-links";
-            evalLinks(element.subpages, subList);
+            evalLinks(element.subpages, subList, subLevel + 1);
             parentElement.appendChild(subList);
         }
     });
@@ -63,7 +75,7 @@ function load() {
     div_links = document.getElementById("sidebar-links");
     iframe_content = document.getElementById("manual-content");
 
-    evalLinks(links, div_links);
+    evalLinks(links, div_links, 0);
 
     try {
         const queryParams = new URLSearchParams(window.location.search);
