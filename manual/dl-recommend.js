c526dba677ebd91a489cbab363d551d4b815c90a
counter185 <33550839+counter185@users.noreply.github.com>
2026-02-14 22:05:54 +0100

    make the download for the current os larger

---
+++ manual/dl-recommend.js
@@ -0,0 +1,25 @@
+function recommendDownload() {
+    var ua = navigator.userAgent;
+    
+    var matchList = [ 
+        ["dl-winx64", "Win64"], 
+        ["dl-winx86", "WOW64"],
+        ["dl-winx86", "Windows NT"],
+        ["dl-winx86", "Win32"],
+        ["dl-linux", "Linux"],
+        ["dl-linux", "X11"],
+        ["dl-mac", "OS X"],
+        ["dl-android", "Android"],
+    ];
+
+    for (var i = 0; i < matchList.length; i++) {
+        if (ua.indexOf(matchList[i][1]) != -1) {
+            var element = document.getElementById(matchList[i][0]);
+            element.style = "font-size: 1.6em; font-weight: bold;";
+            element.innerHTML = "» " + element.innerHTML;
+            break;
+        }
+    }
+}
+
+recommendDownload();
