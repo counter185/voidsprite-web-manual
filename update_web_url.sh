fc94d939ca38b900336ab15148c063eed033323c
counter185 <33550839+counter185@users.noreply.github.com>
2026-08-12 17:49:12 +0200

    i don't know maybe you want curl?

--- update_web_url.sh
+++ update_web_url.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-[[ $(wget https://cntrpl.itch.io/voidsprite -O -) =~ \&quot\;https:\/\/[a-z\.\-]+itch\.zone\/html\/([0-9]+)\/[a-zA-Z0-9\/\?=\.]+\&quot\; ]] && ITCHID=${BASH_REMATCH[1]}
+[[ $(curl https://cntrpl.itch.io/voidsprite) =~ \&quot\;https:\/\/[a-z\.\-]+itch\.zone\/html\/([0-9]+)\/[a-zA-Z0-9\/\?=\.]+\&quot\; ]] && ITCHID=${BASH_REMATCH[1]}
 echo new itch id is $ITCHID
 if [[ $ITCHID =~ ([0-9]+) ]]; then
   sed -i -E "s*src=\"[^\"]+\" allowfullscreen*src=\"https://itch.io/embed-upload/$ITCHID?color=040404\" allowfullscreen*g" ./manual/run_web.html
